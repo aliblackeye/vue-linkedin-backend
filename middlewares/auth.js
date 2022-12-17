a4ad770d907a8a09f9cf83f24278959ddbf0b7e8
aliblackeye <ali.blackeye@hotmail.com>
2022-12-18 02:03:38 +0300

    auth middleware update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,9 +2,9 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   try {
-    let token = req.headers.get("Authorization");
+    let token = await req.headers["Authorization"].split(" ")[1];
     if (!token) return res.status(403).json({ message: "Erişim reddedildi!" });
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
